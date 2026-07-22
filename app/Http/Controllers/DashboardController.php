05ef3bbfc655778ee3fcb24db4f13c844cd35cea
mmilanmihajlovic <mmilan.mihajlovic@gmail.com>
2026-07-22 02:42:25 +0200

    feat(faza1): app/Http/Controllers/DashboardController.php

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,6 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class DashboardController extends Controller {
+    public function index(Request $request) { return view('dashboard'); }
+}
