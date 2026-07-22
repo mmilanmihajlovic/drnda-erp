4606199f73ece048443870be9518b4a1134b5377
mmilanmihajlovic <mmilan.mihajlovic@gmail.com>
2026-07-22 16:01:26 +0200

    scaffold: add Laravel 12 config files

---
+++ config/app.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Facade;
+
+return [
+    'name' => env('APP_NAME', 'DRNDA ERP'),
+    'env' => env('APP_ENV', 'production'),
+    'debug' => (bool) env('APP_DEBUG', false),
+    'url' => env('APP_URL', 'http://localhost'),
+    'timezone' => 'Europe/Belgrade',
+    'locale' => 'en',
+    'fallback_locale' => 'en',
+    'faker_locale' => 'en_US',
+    'cipher' => 'AES-256-CBC',
+    'key' => env('APP_KEY'),
+    'previous_keys' => [],
+    'maintenance' => ['driver' => 'file'],
+    'providers' => Illuminate\Support\ServiceProvider::defaultProviders()->merge([
+        App\Providers\AppServiceProvider::class,
+    ])->toArray(),
+    'aliases' => Facade::defaultAliases()->merge([])->toArray(),
+];
