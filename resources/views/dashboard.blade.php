8812e23342dbbfc8f163f0b6b89a74bee0ab8ea5
mmilanmihajlovic <mmilan.mihajlovic@gmail.com>
2026-07-22 16:04:30 +0200

    scaffold: dashboard.blade.php

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,6 @@
+<x-layouts.app>
+    <div class="p-6">
+        <h1 class="text-2xl font-bold text-white mb-2">Dashboard</h1>
+        <p class="text-gray-400">Dobrodošli u DRNDA ERP 3.0</p>
+    </div>
+</x-layouts.app>
